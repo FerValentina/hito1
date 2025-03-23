a5c57d2faaadf15e6389593b5add21b94f8625b7
Fernanda <fvalentina.gf@gmail.com>
2025-03-23 18:47:21 -0300

    añadiendo todo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './views/Home';
+
+const App = () => {
+  return (
+    <>
+    
+      <Navbar />
+      
+        <Home />
+     
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App;
